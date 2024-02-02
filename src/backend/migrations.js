f23c2b38ce1d59bc92260b81f54517a27ec64df2
Akif Tariq <akiftariq@gmail.com>
2024-02-03 01:19:34 +0500

    Made async

--- src/backend/migrations.js
+++ src/backend/migrations.js
@@ -1,72 +1,56 @@
-import fs from 'fs';
+import fs from 'node:fs/promises';
+import process from 'process';
 import * as db from './db/db.js';
 import path from 'path';
 
-const directoryPath = './migrations';
+let directoryPath = './migrations';
 
-db.query(
-	`
-	CREATE SCHEMA IF NOT EXISTS migrations;
-	CREATE TABLE IF NOT EXISTS migrations.migrations(
+const totalArguments = process.argv.length;
+const directoryParamIndex = process.argv.findIndex((e) => e === '-d');
+
+if (directoryParamIndex >= 0) {
+	if (totalArguments <= directoryParamIndex + 1) {
+		console.error('Bad arguments.');
+		console.log('Exiting.');
+		process.exit(0);
+	}
+	directoryPath = process.argv[directoryParamIndex + 1];
+}
+
+console.log();
+console.log('Reading scripts from directory: ' + directoryPath);
+console.log('--------------------');
+
+await db.query(`CREATE SCHEMA IF NOT EXISTS migrations;
+CREATE TABLE IF NOT EXISTS migrations.migrations(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(255) NOT NULL
-);`
-)
-	.then(() => {
-		db.query('SELECT id, "name" FROM migrations.migrations ORDER BY id;')
-			.then((existingMigrations) => {
-				fs.readdir(directoryPath, (fdError, files) => {
-					if (fdError) {
-						console.error('Error reading directory:', fdError);
-						return;
-					}
-					for (const file of files) {
-						if (path.extname(file) === '.sql') {
-							const filePath = path.join(directoryPath, file);
-							if (existingMigrations.rows && (existingMigrations.rowCount === 0 || existingMigrations.rows.find((e) => e.name === file < 0))) {
-								fs.readFile(filePath, 'utf8', (fError, fileContents) => {
-									if (fError) {
-										console.error('Error reading file:', fError);
-										return;
-									}
-									console.log(`Executing migration: ${filePath}`);
-									db.query(fileContents)
-										.then(() => {
-											console.log('Exuceted migration: ' + file);
-											console.log('Saving migration in db: ' + file);
-											db.query(
-												`INSERT INTO migrations.migrations
-									(id, "name")
-									VALUES(nextval('migrations.migrations_id_seq'::regclass), $1);`,
-												[file]
-											)
-												.then(() => {
-													console.log('Saved migration in db: ' + file);
-													console.log('--------------------');
-												})
-												.catch((errMigration) => {
-													console.log(errMigration);
-													return;
-												});
-										})
-										.catch((errMigration) => {
-											console.log(errMigration);
-											return;
-										});
-								});
-							} else {
-								console.log(filePath + ' is already executed.');
-							}
-						}
-					}
-				});
-			})
-			.catch((e2) => {
-				console.log(e2);
-				return;
-			});
-	})
-	.catch((e1) => {
-		console.log(e1);
-		return;
-	});
+);`);
+
+const existingMigrations = await db.query('SELECT id, "name" FROM migrations.migrations ORDER BY id;');
+
+const files = await fs.readdir(directoryPath);
+
+for (const file of files) {
+	if (path.extname(file) === '.sql') {
+		const filePath = path.join(directoryPath, file);
+		if (existingMigrations.rows && (existingMigrations.rowCount === 0 || existingMigrations.rows.findIndex((e) => e.name === file) < 0)) {
+			const fileContents = await fs.readFile(filePath, { encoding: 'utf8' });
+			console.log(`Executing migration: ${filePath}`);
+			await db.query(fileContents);
+			console.log('Exuceted migration: ' + file);
+			console.log('Saving migration in db: ' + file);
+			await db.query(
+				`INSERT INTO migrations.migrations
+			(id, "name")
+			VALUES(nextval('migrations.migrations_id_seq'::regclass), $1);`,
+				[file],
+			);
+			console.log('Saved migration in db: ' + file);
+			console.log('--------------------');
+		} else {
+			console.log(filePath + ' is already executed.');
+			console.log('--------------------');
+		}
+	}
+}
